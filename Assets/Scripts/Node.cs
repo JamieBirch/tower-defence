484e45e82f5edca0033faf659dfb3b779019e969
JamieBirch <jamie.d.birch@gmail.com>
2022-12-21 12:10:37 +0100

    Add building system
    highlight nodes, implement adding turrets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (this.turret != null)
+        {
+            Debug.Log("Can't build there");
+            return;
+        }
+
+        //Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+}
