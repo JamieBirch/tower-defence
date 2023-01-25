4c63bcfccddb1f731545b7df699aa1c2a53e2ea8
JamieBirch <jamie.d.birch@gmail.com>
2023-01-25 11:45:16 +0100

    Add laser beamer
    Add laser beamer

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -16,8 +16,8 @@ public class BuildManager : MonoBehaviour
 
     }
 
-    public GameObject standardTurretPrefab;
-    public GameObject missileLauncherPrefab;
+    //public GameObject standardTurretPrefab;
+    //public GameObject missileLauncherPrefab;
 
     public GameObject buildEffect;
 
--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,6 +4,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint standardTurret;
     public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
     
     BuildManager buildManager;
 
@@ -21,6 +22,12 @@ public class Shop : MonoBehaviour
     {
         Debug.Log("Missile Launcher");
         buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    public void SelectLaserBeamer()
+    {
+        Debug.Log("Laser Beamer");
+        buildManager.SelectTurretToBuild(laserBeamer);
     }
 
 }
