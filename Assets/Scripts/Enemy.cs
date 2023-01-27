e22d0a233197e02807f43c92fe75de3cb2ae0395
JamieBirch <jamie.d.birch@gmail.com>
2023-01-27 12:58:30 +0100

    Slow down lasered enemy
    Slow down lasered enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
-    public int health = 100;
+    public float startSpeed = 10f;
+    [HideInInspector]
+    public float speed;
+    public float health = 100;
     public int reward = 50;
-
-    private Transform target;
-    private int waypointIndex = 0;
 
     void Start()
     {
-        target = Waypoints.waypoints[0];
+        speed = startSpeed;
     }
 
-    public void TakeDamage (int amount)
+    public void TakeDamage (float amount)
     {
         health -= amount;
 
@@ -33,32 +32,8 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    void Update()
+    public void Slow(float slowPct)
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-        
-        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
-        {
-            GetNextWaypoint();
-        }
-    }
-
-    private void GetNextWaypoint()
-    {
-        if (waypointIndex >= Waypoints.waypoints.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-        waypointIndex++;
-        target = Waypoints.waypoints[waypointIndex];
-    }
-
-    void EndPath ()
-    {
-        PlayerStats.Lives--;
-        Destroy(gameObject);
-
+        speed = startSpeed * (1f - slowPct);
     }
 }
